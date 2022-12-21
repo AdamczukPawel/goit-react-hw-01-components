330e99bcf6aa00d490dd744a4bc7568e9749e1b2
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2022-12-21 17:37:26 +0100

    Ukończenie 1 zadania

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import Profile from './Profile';
+import user from '../utilities/user.json';
+
+export const App = () => {
+  return (
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </>
+  );
+};
