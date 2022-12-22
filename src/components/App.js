febb35ccf7744f637cea6fc8f153eac810a26fcf
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2022-12-22 20:15:27 +0100

    Finished whole homework

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import Profile from './Profile';
 import Statistics from './Statistics';
 import Friends from './FriendList';
+import TransactionHistory from './TransactionHistory';
 import user from '../utilities/user.json';
 import data from '../utilities/data.json';
 import friends from '../utilities/friends.json';
+import transactions from '../utilities/transactions.json';
 
 export const App = () => {
   return (
@@ -16,7 +18,9 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
       <Friends friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -1,25 +1,15 @@
-import clsx from 'clsx';
 import css from './FriendList.module.css';
+import FriendListItem from './FriendListItem';
 
 const FriendsList = ({ friends }) => {
   return (
     <ul className={`${css.friendList}`}>
       {friends.map(friend => (
-        <li className={`${css.item}`} key={friend.id}>
-          <span
-            className={clsx(
-              'css.status',
-              [friend.isOnline] ? 'css.online' : 'css.offline'
-            )}
-          ></span>
-          <img
-            className={`${css.avatar}`}
-            src={friend.avatar}
-            alt="User avatar"
-            width="48"
-          />
-          <p className={`${css.name}`}>{friend.name}</p>
-        </li>
+        <FriendListItem
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+        />
       ))}
     </ul>
   );
