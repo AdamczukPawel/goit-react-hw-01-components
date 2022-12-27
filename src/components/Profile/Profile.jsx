da4884d39c228173ba5c8908656807181261608d
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2022-12-27 17:29:07 +0100

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -34,7 +34,11 @@ Profile.propTypes = {
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
-  stats: PropTypes.objectOf(PropTypes.shape({ followers:PropTypes.string.isRequired, views:PropTypes.number.isRequired, likes:PropTypes.number.isRequired })).isRequired,
+  stats: PropTypes.objectOf(PropTypes.shape({ 
+    followers:PropTypes.string.isRequired, 
+    views:PropTypes.number.isRequired, 
+    likes:PropTypes.number.isRequired }))
+    .isRequired,
 };
 
 export default Profile;
